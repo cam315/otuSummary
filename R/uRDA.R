5114664feb2635dd00037784c877bd675b55731c
Sizhong Yang <37700267+camel315@users.noreply.github.com>
2018-05-03 21:26:53 +0200

    Update uRDA.R

--- R/uRDA.R
+++ R/uRDA.R
@@ -1,18 +1,18 @@
-uRDA <- function(otutab,env = NULL, mySite=NULL, scale=FALSE, biplot=FALSE, choices=1:2, display = "sites", showsp=TRUE, 
-                 legPos ='bottomright',saveplot=FALSE, ...){
-  if(scale) obj <- rda(otutab, scale= TRUE)
-  else obj <- rda(otutab)
+uRDA <- function(otutab,env = NULL, mySite=NULL, scale=FALSE, biplot=FALSE, choices=1:2, display = "sites", 
+                 showsp=TRUE, legPos ="bottomright",saveplot=FALSE, ...){
+  obj <- rda(otutab, scale = scale)
   print(summary(obj))
-  xlbl = paste0('PC1 ', getpropX(obj))
-  ylbl = paste0('PC2 ', getpropY(obj))
+  axes = paste0("PC", choices)
+  xlbl = paste(axes[1], getpropX(obj))
+  ylbl = paste(axes[2], getpropY(obj))
   pchx = pchChoose(mySite)
   my.col = colChoose(mySite)
-  if(is.null(mySite)) {my.site = as.factor(rep('site', nrow(otutab)))
+  if(is.null(mySite)) {my.site = as.factor(rep("site", nrow(otutab)))
   legends = FALSE
   } else { my.site = mySite
   legends= TRUE}
   if(biplot){
-    biplot(obj, scaling = 3, type = c("text", "points"), cex.lab=1.5,cex.axis=1.2,
+    biplot(obj, scaling = 3, choices = choices, type = c("text", "points"), cex.lab=1.5,cex.axis=1.2,
            main = "Unconstrained PCA biplot - scaling 3")
     if(legends){
       legs = levels(mySite)
@@ -25,16 +25,16 @@ uRDA <- function(otutab,env = NULL, mySite=NULL, scale=FALSE, biplot=FALSE, choi
       dev.copy2pdf(file = paste0("Unconstained_RDA_plot_",deparse(substitute(otutab)),".pdf"),
                    useDingbats = FALSE)
     }
-  }else{
+  } else {
     plot(obj,type="n",xlab = xlbl, ylab = ylbl, cex.lab = 1.5, cex.axis=1.2, choices=choices,
          main = paste0("Unconstrained RDA plot for ", deparse(substitute(otutab))))
     abline(h=0,v=0,lty="dashed",col="grey")
-    points(obj, display="si",cex=1.2,pch= pchx[my.site],
+    points(obj, display="si",cex=1.2,pch= pchx[my.site], choices = choices,
            col=my.col[my.site], bg=my.col[my.site], ...)
-    text(obj,display="si", cex=0.9,col=my.col[my.site],adj=-0.1, ...)
+    text(obj,display="si", choices = choices, cex=0.9,col=my.col[my.site],adj=-0.1, ...)
     if(showsp){
-      points(obj, display="sp", cex=0.8, pch= '+', col='red')
-      text(obj, display="sp", cex=0.9, col = my.col[my.site],adj=-0.1)
+      points(obj, display="sp", choices = choices, cex=0.8, pch= '+', col='red')
+      text(obj, display="sp", choices = choices, cex=0.9, col = my.col[my.site],adj=-0.1)
     }
     if(legends){
       legs = levels(mySite)
