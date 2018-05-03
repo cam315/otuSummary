67ab03455164a88451119716e120e35a1f08f24f
Sizhong Yang <37700267+camel315@users.noreply.github.com>
2018-05-03 22:23:33 +0200

    Update getpropY.R

--- R/getpropY.R
+++ R/getpropY.R
@@ -1,4 +1,4 @@
-getpropY <- function(obj, choices = 1:2){
+getpropY <- function(obj, choices){
   eigs <- eigenvals(obj)
   pop <- round(eigs/sum(eigs)*100, 2)
   laby <- paste0('(',pop[choices[2]],'%)')
