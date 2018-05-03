fde75144758dc2b310242ed66fc3d45aa9e37d34
Sizhong Yang <37700267+camel315@users.noreply.github.com>
2018-05-03 22:18:27 +0200

    Update getpropX.R

--- R/getpropX.R
+++ R/getpropX.R
@@ -1,6 +1,6 @@
-getpropX <- function(obj){
+getpropX <- function(obj, choices=1:2){
   eigs <- eigenvals(obj)
   pop <- round(eigs/sum(eigs)*100, 2)
-  labx <- paste0("(", pop[1], "%)")
+  labx <- paste0("(", pop[choices[1]], "%)")
   return(labx)
 }
