dc34acd480c45ee08ccf4f64c7705e4b1b50cf61
Sizhong Yang <37700267+camel315@users.noreply.github.com>
2018-05-04 13:22:51 +0200

    Update cCCA.R

--- R/cCCA.R
+++ R/cCCA.R
@@ -1,23 +1,23 @@
 cCCA <- function(otutab, env, mySite=NULL,scale=FALSE,choices=1:2,display = "sites",
                  showsite = FALSE,legPos ='bottomright', saveplot = FALSE, ...){
   obj <- cca(otutab, env, scale = scale)
-  axes = paste0('CCA', choices)
+  axes = paste0("CCA", choices)
   xlbl = paste(axes[1], getpropX(obj, choices))
   ylbl = paste(axes[2], getpropY(obj, choices))
   pchx <- pchChoose(mySite)
   my.col <- colChoose(mySite)
-  if(is.null(mySite)) {my.site = as.factor(rep('site',nrow(otutab)))
+  if(is.null(mySite)) {my.site = as.factor(rep("site", nrow(otutab)))
   legends = FALSE
   } else { my.site = mySite
   legends= TRUE}
   print(summary(obj))
-  plot(obj, main=paste0("Constrained CCA plot for ",deparse(substitute(otutab))),
+  plot(obj, main=paste("Constrained CCA plot for",deparse(substitute(otutab))),
        choices = choices, cex.lab = 1.5, cex.axis=1.2,xlab = xlbl, ylab = ylbl)
-  spe.sc <- scores(obj, choices = choices, scaling=2,display='sp')
+  spe.sc <- scores(obj, choices = choices, scaling=2,display="sp")
   arrows(0,0, spe.sc[,1], spe.sc[,2],length=0, lty=1,col='red')
   text(obj,display="si", choices = choices, cex=0.9,col=my.col[my.site],adj=-0.1)
   if(showsite){
-    sit.sc <- scores(obj, choices = choices, scaling=2, display='si')
+    sit.sc <- scores(obj, choices = choices, scaling=2, display="si")
     points(sit.sc[,1],sit.sc[,2],pch= pchx[my.site],col=my.col[my.site])
     text(sit.sc[,1],sit.sc[,2], labels=rownames(sit.sc),
          cex=0.9,col=my.col[my.site],adj=-0.1)
