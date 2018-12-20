bb977ec5def082252fa7d07ed6c9773420b376ea
cam315 <noasark@163.com>
2018-12-20 09:46:36 +0100

    Update subOTU.R

--- R/subOTU.R
+++ R/subOTU.R
@@ -13,7 +13,11 @@ subOTU <- function(otutab, siteInCol = TRUE, taxhead = NULL, percent = TRUE, cho
     tax = otutab[, which(colnames(otutab) == taxhead)]
     tax = levels(tax)[tax]
   } else {stop("The taxonomy marker is incorrect, please check it")}
-  if(!percent) {per = sweep(count, 2, colSums(count), "/")*100
+  if(!percent) {
+    per = sweep(count, 2, colSums(count), "/")*100
+    per[is.na(per)] <- 0
+    cat("Warning! detected column with all values of zero in column:")
+    cat(colnames(per)[colSums(is.na(per)) > 0])
   } else { per= count}
   if(grepl(choose,"Rare|rare")){
     ids <- which(rowMeans(per)< threshold)
@@ -48,8 +52,7 @@ subOTU <- function(otutab, siteInCol = TRUE, taxhead = NULL, percent = TRUE, cho
     } else {output = count}
   } else {print("Invalid outype, will output relative abundance without taxonomy")
     output = otutabc }
-  message(paste("The outype data have", dim(output)[1],"rows and",dim(output)[2],"columns"))
-  print(dim(output))
+  message(paste("The output data frame contains", dim(output)[1],"rows and",dim(output)[2],"columns"))
   if(write){
     filename = paste0("subOtuTab_",deparse(substitute(otutab)),".txt")
     message(paste0("The subsampled table ","\"",filename,"\"", " will be written out"))
