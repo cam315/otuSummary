ec4a61529b53cea2e0364c34013c1c9f5e12498a
cam315 <noasark@163.com>
2018-12-20 10:21:31 +0100

    Update subOTU.R

--- R/subOTU.R
+++ R/subOTU.R
@@ -5,22 +5,26 @@ subOTU <- function(otutab, siteInCol = TRUE, taxhead = NULL, percent = TRUE, cho
     message("Site headers not in columns, will be transposed")
     otutab = typeConvert(otutab=as.data.frame(t(otutab)),taxhead = taxhead)}
   if(is.null(taxhead)){
-    message("The input dataset does not contain a taxonomy column")
+    message("The input dataset does not contain a taxonomy column.")
     count = otutab
   } else if(taxhead %in% names(otutab)) {
-    message("There is one column given taxonomy in the data set")
+    message("There is one column given taxonomy in the data set.")
     count = otutab[,which(colnames(otutab) != taxhead)]
     tax = otutab[, which(colnames(otutab) == taxhead)]
     tax = levels(tax)[tax]
-  } else {stop("The taxonomy marker is incorrect, please check it")}
-  if(!percent) {per = sweep(count, 2, colSums(count), "/")*100
+  } else {stop("The taxonomy marker is incorrect, please check it.")}
+  if(!percent) {
+    per = sweep(count, 2, colSums(count), "/")*100
+    per[is.na(per)] <- 0 
+    cat("Warning! detected column with all values of zero in column:")
+    cat(colnames(per)[colSums(is.na(per)) > 0])
   } else { per= count}
   if(grepl(choose,"Rare|rare")){
     ids <- which(rowMeans(per)< threshold)
   } else if (grepl(choose,"abundant")){
     ids <- which(rowMeans(per) >= threshold)
   } else if (grepl(choose,"All|all")){
-    message("Keep all biosphere, but the empty rows will be removed")
+    message("Keep all biosphere, but the empty rows will be removed.")
     ids <- which(rowMeans(per) > 0)
   }
   otutabc <- per[ids,]
@@ -34,7 +38,7 @@ subOTU <- function(otutab, siteInCol = TRUE, taxhead = NULL, percent = TRUE, cho
     taxc <- taxc[ord]
   }
   if(grepl(outype, 'Relabund|relabund')){
-    message("The outype is the relative abundance data")
+    message("The outype is the relative abundance data.")
     if(is.null(taxhead)) { output = per
     } else if (taxhead %in% names(otutab)){
       output <- data.frame(otutabc,taxonomy=taxc)
@@ -48,8 +52,7 @@ subOTU <- function(otutab, siteInCol = TRUE, taxhead = NULL, percent = TRUE, cho
     } else {output = count}
   } else {print("Invalid outype, will output relative abundance without taxonomy")
     output = otutabc }
-  message(paste("The outype data have", dim(output)[1],"rows and",dim(output)[2],"columns"))
-  print(dim(output))
+  message(paste("The output data frame contains", dim(output)[1],"rows and",dim(output)[2],"columns"))
   if(write){
     filename = paste0("subOtuTab_",deparse(substitute(otutab)),".txt")
     message(paste0("The subsampled table ","\"",filename,"\"", " will be written out"))
