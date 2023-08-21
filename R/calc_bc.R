f9b6db3f9780d7d60093d5754f05b617508f57ec
Sizhong Yang <3144409+cam315@users.noreply.github.com>
2023-08-21 23:50:13 +0200

    Update calc_bc.R

--- R/calc_bc.R
+++ R/calc_bc.R
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-# script to calculate Bray-Curtis dissimilarity matrix
-## BC is calculated based on the function (BCij = 1 – (2*Cij) / (Si + Sj)), where
-## Cij: The sum of the lesser values for the species found in each site.
-## Si: The total number of specimens counted at site i
-## Sj: The total number of specimens counted at site
-
-#calc_bc <- function(df){
-#  ## please note that the OTU table (df) must be with sites in rows and species in columns
-#  nr = nrow(df)
-#  si = rownames(df)
-#  mat = matrix(nrow=nr, ncol=nr, byrow=FALSE)
-#  colnames(mat) = rownames(mat) = si
-#  ## calculate pairwise BC
-#  for (i in 1:(nr-1)){
-#    for (k in (i+1):nr){
-#    tmp = df[i:k,]
-#    #bc = sum(apply(tmp, 2, function(x) abs(max(x)-min(x))))/sum(tmp)
-#    bc = sum(abs(tmp[1,] - tmp[2,])/sum(tmp))
-#    mat[k,i] = bc
-#   }   
-#  }
-#  mat2 = as.dist(mat)
-#  return(mat2)
-#}
-
-=======
->>>>>>> master
 calc_bc <- function(df){
     ## please note that the OTU table (df) must be with sites in rows and species in columns
     nr = nrow(df)
