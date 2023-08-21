cf27421f6f7e76207a2a0d8a1feb48f72f9f6bd4
Sizhong Yang <3144409+cam315@users.noreply.github.com>
2023-08-21 23:53:29 +0200

    Update pbray.R

--- R/pbray.R
+++ R/pbray.R
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#pbray <- function(allComm, subComm, tolower = TRUE) {
-#  allComm <- as.matrix(allComm)
-#  subComm <- as.matrix(subComm)
-#  nr <- nrow(allComm)
-#  mat <- matrix(0, nrow = nr, ncol = nr)
-#  dimnames(mat) <- list(rownames(allComm),rownames(allComm))
-#  tmp <- vector(mode = "numeric", length = nr*(nr-1)/2)
-#  k = 1
-#  for (i in 1:(nr-1)){
-#    Pi <- allComm[i,]
-#    Ri <- subComm[i,]
-#    for (j in (i+1):nr) {
-#      denominator <- sum(Pi,allComm[j,])
-#      numerator <- sum(abs(Ri - subComm[j,]))
-#      tmp[k] <- numerator/denominator
-#      k = k+1
-#    }
-#    tmp
-#    rm(denominator, numerator)
-#  }
-#  mat[lower.tri(mat)] <- tmp
-#  rm(tmp,k)
-#  distmatrix <- as.dist(mat)
-#  if(!tolower) distmatrix <- as.matrix(distmatrix)
-#  invisible(distmatrix)
-#}
-=======
->>>>>>> master
-
 pbray <- function(allComm, subComm, tolower = TRUE) {
   allComm <- as.matrix(allComm)
   subComm <- as.matrix(subComm)
@@ -38,10 +8,6 @@ pbray <- function(allComm, subComm, tolower = TRUE) {
   denominator = as.dist(outer(rsum, rsum, FUN="+"))
   numerator = dist(subComm, method = 'minkowski', p =1) ## will cal |xi -yi|
   mat = numerator/denominator
-<<<<<<< HEAD
-#  mat[lower.tri(mat)] <- tmp
-=======
->>>>>>> master
   distmatrix <- as.dist(mat)
   if(!tolower) distmatrix <- as.matrix(distmatrix)
   invisible(distmatrix)
